0704387719a15d5692e71b09add9f73ddbf14d14
Adam Svoboda <eidamsvoboda@gmail.com>
2017-06-06 14:36:29 +0200

    add app token to requests via interceptor

--- app/src/main/java/com/eidamsvoboda/meteorites/Api.java
+++ app/src/main/java/com/eidamsvoboda/meteorites/Api.java
@@ -1,8 +1,13 @@
 package com.eidamsvoboda.meteorites;
 
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -22,11 +27,30 @@ public class Api {
 
 	static ServerService get() {
 		if (serverService == null) {
+
+			Interceptor tokenInterceptor = new Interceptor() {
+				@Override public Response intercept(Chain chain) throws IOException {
+					Request original = chain.request();
+					HttpUrl originalUrl = original.url();
+
+					HttpUrl tokenUrl = originalUrl.newBuilder()
+							.addQueryParameter("$$app_token",TOKEN)
+							.build();
+
+					Request.Builder requestBuilder = original.newBuilder()
+							.url(tokenUrl);
+
+					Request tokenRequest = requestBuilder.build();
+					return  chain.proceed(tokenRequest);
+				}
+			};
+
 			HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
 			loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
 			OkHttpClient client = new OkHttpClient.Builder()
 					.addInterceptor(loggingInterceptor)
+					.addInterceptor(tokenInterceptor)
 					.build();
 
 			Retrofit retrofit = new Retrofit.Builder()
@@ -41,7 +65,7 @@ public class Api {
 	}
 
 	interface ServerService {
-		@GET("resource/y77d-th95.json?$where=year >= '2011-01-01T00:00:00.000'&$$app_token="+TOKEN)
+		@GET("resource/y77d-th95.json?$where=year >= '2011-01-01T00:00:00.000'")
 		Call<List<Meteorite>> getMeteorites();
 	}
 }
