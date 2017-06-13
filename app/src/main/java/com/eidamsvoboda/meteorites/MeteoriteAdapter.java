f44d3d3f04de4d24efa3b6e2490b6354cb54fe32
Adam Svoboda <eidamsvoboda@gmail.com>
2017-06-13 10:02:26 +0200

    use List instead of Realm List remove unnecessary String.format

--- app/src/main/java/com/eidamsvoboda/meteorites/MeteoriteAdapter.java
+++ app/src/main/java/com/eidamsvoboda/meteorites/MeteoriteAdapter.java
@@ -7,9 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.realm.RealmList;
 
 /**
  * Created by eidamsvoboda on 05/06/2017.
@@ -17,32 +18,11 @@ import io.realm.RealmList;
 
 public class MeteoriteAdapter extends RecyclerView.Adapter<MeteoriteAdapter.ViewHolder> {
 
-	class ViewHolder extends RecyclerView.ViewHolder {
-		@BindView(R.id.name) TextView nameView;
-		@BindView(R.id.mass) TextView massView;
-		@BindView(R.id.location) TextView locationView;
-		@BindView(R.id.date) TextView dateView;
-
-		public ViewHolder(View view) {
-			super(view);
-			ButterKnife.bind(this, view);
-			view.setOnClickListener(new View.OnClickListener() {
-				@Override public void onClick(View v) {
-					recyclerItemClickListener.onRecyclerItemClick(meteorites.get(getAdapterPosition()));
-				}
-			});
-		}
-	}
-
-	public interface RecyclerItemClickListener {
-		void onRecyclerItemClick(Meteorite meteorite);
-	}
-
-	RealmList<Meteorite> meteorites;
+	List<Meteorite> meteorites;
 	RecyclerItemClickListener recyclerItemClickListener;
 	Context context;
 
-	MeteoriteAdapter(Context context, RealmList<Meteorite> meteorites, RecyclerItemClickListener recyclerItemClickListener) {
+	MeteoriteAdapter(Context context, List<Meteorite> meteorites, RecyclerItemClickListener recyclerItemClickListener) {
 		this.context = context;
 		this.meteorites = meteorites;
 		this.recyclerItemClickListener = recyclerItemClickListener;
@@ -58,7 +38,7 @@ public class MeteoriteAdapter extends RecyclerView.Adapter<MeteoriteAdapter.View
 	public void onBindViewHolder(ViewHolder holder, int position) {
 		Meteorite meteorite = meteorites.get(position);
 		holder.nameView.setText(meteorite.name);
-		holder.massView.setText(String.format(context.getString(R.string.main_mass), meteorite.mass));
+		holder.massView.setText(context.getString(R.string.main_mass, meteorite.mass));
 		holder.locationView.setText(meteorite.reclat + ", " + meteorite.reclong);
 		holder.dateView.setText(meteorite.year.substring(0, meteorite.year.indexOf("-"))); // Show only year, because other data are always the same (1st january 00:00:00)
 	}
@@ -66,5 +46,25 @@ public class MeteoriteAdapter extends RecyclerView.Adapter<MeteoriteAdapter.View
 	@Override
 	public int getItemCount() {
 		return meteorites.size();
+	}
+
+	public interface RecyclerItemClickListener {
+		void onRecyclerItemClick(Meteorite meteorite);
+	}
+	class ViewHolder extends RecyclerView.ViewHolder {
+		@BindView(R.id.name) TextView nameView;
+		@BindView(R.id.mass) TextView massView;
+		@BindView(R.id.location) TextView locationView;
+		@BindView(R.id.date) TextView dateView;
+
+		public ViewHolder(View view) {
+			super(view);
+			ButterKnife.bind(this, view);
+			view.setOnClickListener(new View.OnClickListener() {
+				@Override public void onClick(View v) {
+					recyclerItemClickListener.onRecyclerItemClick(meteorites.get(getAdapterPosition()));
+				}
+			});
+		}
 	}
 }
